b1293d4abbcabac68153cae1b9487468c1f2a810
Riley Rivera <rileyanticode@gmail.com>
2020-09-17 14:09:45 -0700

    Initial commit

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Calculator
+  {
+    static void Main()
+    {
+      Console.WriteLine("Calculator awaiting input...");
+      string input = Console.ReadLine();
+      TreeNode equation = null;
+      try
+      {
+        equation = TreeNode.BuildTree(input);
+      } catch(Exception error)
+      {
+        Console.WriteLine(error.Message);
+      }
+      Console.WriteLine(Operations.SolveTreeNode(equation));
+      Main();
+    }
+  }
+}
---
+++ Calculator/Operations.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+  public static class Operations
+  {
+    public enum OperableEnum
+    {
+      Solved,
+      Add,
+      Subtract,
+      Multiply,
+      Divide
+    }
+
+    public static OperableEnum ParseOperable(char operable)
+    {
+      OperableEnum value = OperableEnum.Solved;
+      switch(operable)
+      {
+        case '+':
+          value = OperableEnum.Add;
+          break;
+        case '-':
+          value = OperableEnum.Subtract;
+          break;
+        case '*':
+          value = OperableEnum.Multiply;
+          break;
+        case '/':
+          value = OperableEnum.Divide;
+          break;
+      }
+      return value;
+    }
+
+    public static double SolveOperation(OperableEnum operable, params string[] numsString)
+    {
+      double[] nums = new double[numsString.Length - 1];
+      double result = 0f;
+      for (int i = 0; i < numsString.Length; i++)
+      {
+        if (i == 0)
+        {
+          result = nums[0];
+          continue;
+        }
+        while (numsString[i - 1].StartsWith('_')) numsString[i - 1].Substring(1);
+        nums[i] = double.Parse(numsString[i - 1]);
+      }
+      switch (operable)
+      {
+        case OperableEnum.Add:
+          foreach (double num in nums) result += num;
+          break;
+        case OperableEnum.Subtract:
+          foreach (double num in nums) result -= num;
+          break;
+        case OperableEnum.Multiply:
+          foreach (double num in nums) result *= num;
+          break;
+        case OperableEnum.Divide:
+          if (nums.Length != 1) throw new Exception("You cannot divide more than 2 things at once.");
+          result /= nums[0];
+          break;
+        case OperableEnum.Solved:
+          break;
+        default:
+          throw new Exception("Operations.SolveOperation was given an operable it cannot yet handle.");
+      }
+      return result;
+    }
+
+    public static double SolveTreeNode(TreeNode treeNode)
+    {
+      string[] children = new string[treeNode.children.Keys.Count];
+      int i = 0;
+      foreach (string child in treeNode.children.Keys)
+      {
+        if (treeNode.GetChild(child).operation != OperableEnum.Solved)
+        {
+          children[i] = SolveTreeNode(treeNode.GetChild(child)).ToString();
+        }
+        else children[i] = child;
+        i++;
+      }
+
+      return SolveOperation(treeNode.operation, children);
+    }
+  }
+}
