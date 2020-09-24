25c955ce79f9a9d85a9bdc213200839d84704487
Riley Rivera <rileyanticode@gmail.com>
2020-09-24 13:57:38 -0700

    Added Addition expression

---
+++ Calculator/Expressions/Addition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Expressions
+{
+  class Addition : Expression
+  {
+    public string name => "add";
+    public string[] shorthands => new string[] { "+", "addition" };
+    public string helpMessage => "Adds all arguments together and returns the result.";
+
+    public string RunExpression(params string[] args)
+    {
+      double returnNumber = 0;
+      foreach (string arg in args)
+      {
+        returnNumber += double.Parse(arg);
+      }
+      Console.WriteLine(returnNumber);
+      return returnNumber.ToString();
+    }
+  }
+}
