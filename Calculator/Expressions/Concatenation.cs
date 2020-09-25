0566939e9f865d68d001a09bf475ee28b4bc0912
Riley Rivera <rileyanticode@gmail.com>
2020-09-24 23:52:44 -0700

    Added Concatenation

---
+++ Calculator/Expressions/Concatenation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Expressions
+{
+  class Concatenation : Expression
+  {
+    public string name => "concat";
+    public string[] shorthands => new string[] { "|", "concatenation" };
+    public string helpMessage => "Concatenates all arguments into a singular value.";
+
+    public string RunExpression(params string[] args)
+    {
+      string concatenatedString = "";
+      foreach (string arg in args)
+      {
+        concatenatedString += arg;
+      }
+      Console.WriteLine(concatenatedString);
+      return concatenatedString;
+    }
+  }
+}
