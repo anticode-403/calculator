163a13a08ca9c624ab33bd0f965632dd1e00e32e
Riley Rivera <rileyanticode@gmail.com>
2020-09-24 23:16:02 -0700

    Added Division and Multiplication

---
+++ Calculator/Expressions/Division.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Expressions
+{
+  class Division : Expression
+  {
+    public string name => "divide";
+    public string[] shorthands => new string[] { "/", "division" };
+    public string helpMessage => "Divides two arguments and returns the result.";
+
+    public string RunExpression(params string[] args)
+    {
+      if (args.Length > 2) throw new Exception("divide command was passed more than two arguments.");
+      double denominator = double.Parse(args[0]);
+      double quotient = denominator / double.Parse(args[1]);
+      Console.WriteLine(quotient);
+      return quotient.ToString();
+    }
+  }
+}
---
+++ Calculator/Expressions/Multiplication.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Expressions
+{
+  class Multiplication : Expression
+  {
+    public string name => "multiply";
+    public string[] shorthands => new string[] { "*", "multiplication" };
+    public string helpMessage => "Multiplies all arguments together and returns the result.";
+
+    public string RunExpression(params string[] args)
+    {
+      double returnNumber = 0;
+      foreach (string arg in args)
+      {
+        returnNumber *= double.Parse(arg);
+      }
+      Console.WriteLine(returnNumber);
+      return returnNumber.ToString();
+    }
+  }
+}
