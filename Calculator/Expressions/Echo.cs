d2d630a379aec987e8b023f6460364db2d45d284
Riley Rivera <rileyanticode@gmail.com>
2020-09-24 23:20:22 -0700

    Added Echo command

---
+++ Calculator/Expressions/Echo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator.Expressions
+{
+  class Echo : Expression
+  {
+    public string name => "echo";
+    public string[] shorthands => new string[] {  };
+    public string helpMessage => "Echo all the arguments as a singular string.";
+
+    public string RunExpression(params string[] args)
+    {
+      string echoString = String.Join(' ', args);
+      Console.WriteLine(echoString);
+      return echoString;
+    }
+  }
+}
